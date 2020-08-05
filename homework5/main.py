0172d5955285bafd817487d7681611e9685c1bb5
Светлана <SS.Shevchenko@yandex.ru>
2020-08-05 05:17:53 -0700

    add hw5

--- homework5/main.py
+++ homework5/main.py
@@ -1,1 +1,12 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
 
+from homework5 import settings
+from homework5.spiders.habr import HabrSpider
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(HabrSpider)
+    process.start()
